4751ea1ee99efe543726abefa46739ffe4f37db0
Vs-kishu <91000916+Vs-kishu@users.noreply.github.com>
2023-11-16 10:15:05 +0530

    minor chnages in keys

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "ai-netflix.firebaseapp.com",
-  projectId: "ai-netflix",
-  storageBucket: "ai-netflix.appspot.com",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MSG_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_Measurement_ID,
